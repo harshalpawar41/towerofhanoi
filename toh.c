20dd29f0e43d5667270b18f2256d6bd43480f608
HarshangVirani <52438740+HarshangVirani@users.noreply.github.com>
2020-10-01 00:42:10 +0530

    Changing in tower of Hanoi

--- toh.c
+++ toh.c
@@ -7,15 +7,3 @@ main()
     scanf("%d",&n);
     towerOfHanoi(n, 'A', 'C', 'B');
 }
-
-void towerOfHanoi(int n, char from, char to, char temp)
-{
-    if (n == 1)
-    {
-        printf("Move disk 1 from %c to %c\n", from, to);
-        return;
-    }
-    towerOfHanoi(n-1, from, temp, to);
-    printf("Move disk %d from %c to %c\n", n, from, to);
-    towerOfHanoi(n-1, temp, to, from);
-}
